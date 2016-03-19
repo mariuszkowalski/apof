df50866b682e0e250ab2d086a7ef222e39e231a7
mariuszkowalski <mariusz.kowalski.115@gmail.com>
2016-03-19 13:21:35 +0100

    Corrected some errors

--- apof/add_restaurants.py
+++ apof/add_restaurants.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env Python3
 # -*- coding: utf-8 -*-
-from django.db import models
+from django import setup as django_setup
+django_setup()
 from models import Restaurant, Meal, Ingredients
 
 
@@ -20,5 +22,3 @@ r1 = Restaurant(name='Coloseum',
 				open_at_sun='12:00',
 				close_at_sun='22:00')
 r1.save()
-
-#r1 = Restaurant(name='Coloseum', phone='(67) 352 91 11', open_at_mon='12:00', close_at_mon='22:00', open_at_tue='11:00', close_at_tue='22:00', open_at_wed='11:00', close_at_wed='22:00', open_at_thu='11:00', close_at_thu='22:00', open_at_fri='11:00', close_at_fri='23:00', open_at_sat='11:00', close_at_sat='23:00', open_at_sun='12:00', close_at_sun='22:00')
