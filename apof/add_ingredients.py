adeb40ca3d7cfdbd7eb11251b2be45957e48b0ef
mariuszkowalski <mariusz.kowalski.115@gmail.com>
2016-03-20 21:30:48 +0100

    add_ingredinets, parse_coloseum, are still work in progres, parse_coloseum needs some twiking in regards to differences in lines of the html page.

--- apof/add_ingredients.py
+++ apof/add_ingredients.py
@@ -1,60 +1,45 @@
 #!/usr/bin/env Python3
 # -*- coding: utf-8 -*-
-from django import setup as django_setup
-django_setup()
-from restaurants.models import Restaurant, Meal, Ingredients
 
-ingredients = ['sos podkładowy',
-			'ser',
-			'szynka',
-			'pieczarki',
-			'boczek',
-			'cebula',
-			'kurczak',
-			'salami',
-			'papryka',
-			'oliwki',
-			'tuńczyk',
-			'kukurydza',
-			'krewetki',
-			'czosnek',
-			'pepperoni',
-			'tabasco',
-			'ananas',
-			'mięso mielone',
-			'pomidor',
-			'mięso kebab',
-			'małże',
-			'ogórek świeży',
-			'ser feta',
-			'kapary',
-			'ser pleśniowy',
-			'kiełbasa żywiecka',
-			'ogórek konserwowy',
-			'szpinak',
-			'jajko',
-			'gouda',
-			'lazur',
-			'brie',
-			'sos boloński',
-			'groszek',
-			'brokuły',
-			'ziarno słonecznika',
-			'rukola',
-			'suszone pomidory',
-			'szparagi',
-			'polędwica',
-			'banan',
-			'anchois',
-			'ogórek',
-			'kabanos',
-			'ser favita',
-			'kurki',
-			'przyprawy',
-			'ser pleśniowy',
-			'mozzarella',
-			'świeża rucola',
-			'szynka parmeńska']
+#Read the settings file and initialize the django db.
+import os
+from django import setup as django_setup
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apof.settings.common")
+django_setup()
+
+#Read models from restaurants models and settings from parse_coloseum.
+from restaurants.models import Restaurant, Meal, Ingredients
+from parse_coloseum import BASE_DIR, CSV_DIR, URL
+from parse_coloseum import read_site
+from parse_coloseum import parse_coloseum, parse_name, parse_ingredients, parse_prices
+from parse_coloseum import save_parsed, compare_parsed
+import codecs
+import csv
+
+
+TARGET_DIR = os.path.join(CSV_DIR, 'parsed_main\coloseum_parsed.txt')
+
+
+def open_file():
+	all_pizzas = []
+
+	try:
+		current_file = codecs.open(TARGET_DIR, 'r', 'utf-8')
+	
+	except FileNotFoundError:
+		procesed_data = parse_coloseum(URL)
+		save_parsed(procesed_data)
+		compare_parsed()
+		open_file()
+	
+	else:
+		data = csv.reader(current_file, delimiter=';')
+		
+		for row in data:
+			all_pizzas.append(row)
+		current_file.close()
+		
+		return all_pizzas
 
 
 def populate(alist):
@@ -63,7 +48,10 @@ def populate(alist):
 		i1.save()
 
 def main():
-	populate(ingredients)
+	data = open_file()
+	print(data)
+	print('Everything went ok.')
+	#populate(ingredients)
 
 
 if __name__ == '__main__':
--- apof/parse_coloseum.py
+++ apof/parse_coloseum.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env Python3
 # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup as bs
+from shutil import copyfile
 import urllib.request
 import hashlib
 import codecs
 import os
+import re
 
-#######################################################################################################################
-#
+#----------------------------------------------------------------------------------------------------------------------
 # Settings
-#
-#######################################################################################################################
+#----------------------------------------------------------------------------------------------------------------------
 
 # Path settings.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,17 +21,26 @@ SETTINGS = os.path.join(BASE_DIR, 'apof\settings')
 # Addresses to parse.
 URL = 'http://www.coloseum.pl/menu.html'
 
+#State of debug mode.
+DEBUG = False
+
 
 def read_site(url):
 	raw_content = urllib.request.urlopen(url).read()
 	return bs(raw_content, 'html.parser')
 
 
-#######################################################################################################################
+#----------------------------------------------------------------------------------------------------------------------
 # Parse function with subfunctions.
-#######################################################################################################################
+#----------------------------------------------------------------------------------------------------------------------
 
 def parse_coloseum(url):
+	'''
+	Parses the Coloseum menu.html.
+
+	Returns:
+		list: pizzas_procesed - contains list of all current pizzas from the menu.
+	'''
 	soup = read_site(url)
 	
 	pizzas_procesed = []
@@ -48,7 +57,7 @@ def parse_coloseum(url):
 
 		elif 'class="pizza-opis"' in element:
 
-			current_pizza = parse_ingeredients(i, pizzas_splited, current_pizza)
+			current_pizza = parse_ingredients(i, pizzas_splited, current_pizza)
 			#print (current_pizza)
 			
 		elif 'class="pizza-cena"' in element:
@@ -60,39 +69,94 @@ def parse_coloseum(url):
 
 #Subfunctions of the 'parse_coloseum' function.
 def parse_name(i, pizzas_splited):
+	'''
+	Args:
+		i - index from enumerate method.
+		pizzas_splited - list of lines with raw data from the menu.html page.
+	
+	Returns:
+		current_pizza - list of all properities of pizza
+						(name, spicy, ingredients, prices per size).
+	'''
 	if 'ostra' in pizzas_splited[i+1]:
 		current_pizza = []
 		temp = str(pizzas_splited[i+1])
-		temp = temp[7:-39].strip().lstrip()
-		current_pizza.append(temp)
+		name = re.findall(r"[\w']+", temp)
+
+		if name[3] == 'b':
+			record = str(name[2]).strip().lstrip()
+		else:
+			record = ' '.join(name[2:4]).strip().lstrip()
+
+		current_pizza.append(record)
 		current_pizza.append('tak')
 
 	else:
 		current_pizza = []
 		temp = str(pizzas_splited[i+1])
-		temp = temp[7:-4].strip().lstrip()
-		current_pizza.append(temp)
+		name = re.findall(r"[\w']+", temp)
+
+		if name[3] == 'b':
+			record = str(name[2]).strip().lstrip()
+		else:
+			record = ' '.join(name[2:4]).strip().lstrip()
+
+		current_pizza.append(record)
 		current_pizza.append('nie')
 
 	return current_pizza
 
 
-def parse_ingeredients(i, pizzas_splited, current_pizza):
+def parse_ingredients(i, pizzas_splited, current_pizza):
+	'''
+	Args:
+		i - index from enumerate method.
+		pizzas_splited - list of lines with raw data from the menu.html page.
+		current_pizza - list of all properties of the pizza
+						(name, spicy, ingredients, prices per size).
+
+	Returns:
+		current_pizza - list of all properities of pizza
+						(name, spicy, ingredients, prices per size).
+	'''
+	ing = []
 	temp = str(pizzas_splited[i+1])
-	temp = temp.split(',')
-	for nr, ingr in enumerate(temp):
-		if '<br>' in ingr:
-			subprocess = str(ingr).split('<br>')
-			temp[nr] = ''.join(subprocess).strip().lstrip()
-			current_pizza.append(temp[nr])
-		else:
-			temp[nr] = str(ingr).strip().lstrip()
-			current_pizza.append(temp[nr])
+	temp2 = str(pizzas_splited[i+2])
+	#temp = re.findall(r"[\w']+", temp)
+	temp = re.findall(r"[\+]|[\(][\w]+|[\w]+[\)]|[\w]+[\,]|[\w]+", temp)
+	temp2 = re.findall(r"[\+]|[\(][\w]+|[\w]+[\)]|[\w]+[\,]|[\w]+", temp2)
+
+	ing.extend(temp)
+	if len(temp2) > 1:
+		ing.extend(temp2[1:])
+	
+	ingredient = []
+	for element in ing:
+		if element != 'br':
+			ingredient.append(element)
+		elif element != 'td':
+			ingredient.append(element)
+
+	ingredient = ' '.join(ingredient)
+	ingredient = ingredient.split(',')
+	current_pizza.extend(ingredient)
 
 	return current_pizza
 
 
 def parse_prices(i, pizzas_splited, current_pizza, pizzas_procesed, sentinel):
+	'''
+	Args:
+		i - index from enumerate method.
+		pizzas_splited - list of lines with raw data from the menu.html page.
+		current_pizza - list of all properties of the pizza
+						(name, spicy, ingredients, prices per size).
+		pizzas_procesed - main list of the all available pizzas.
+		sentinel - sentinel value for appending current pizza to all pizzas list.
+							
+	Returns:
+		pizzas_procesed - main list of the all available pizzas.
+	'''
 	sentinel += 1		
 	temp = str(pizzas_splited[i+1])
 	temp = temp[3:8]
@@ -105,11 +169,16 @@ def parse_prices(i, pizzas_splited, current_pizza, pizzas_procesed, sentinel):
 	return pizzas_procesed, sentinel
 
 
-#######################################################################################################################
+#----------------------------------------------------------------------------------------------------------------------
 # Saveing parsed data to the file in CSV format.
-#######################################################################################################################
+#----------------------------------------------------------------------------------------------------------------------
 
-def save_parsed(text):
+def save_parsed(a_list):
+	'''
+	Saves parsed data to file in CSV format.
+	If data file is present in main directory,
+	creates file in aux directory.
+	'''
 	dir_1 = os.path.join(CSV_DIR, 'parsed_main')
 	dir_2 = os.path.join(CSV_DIR, 'parsed_aux')
 	target_1 = os.path.join(dir_1, 'coloseum_parsed.txt')
@@ -126,13 +195,13 @@ def save_parsed(text):
 		target = target_2
 	
 	with codecs.open(target, 'w', 'utf-8') as myfile:
-		for element in text:
+		for element in a_list:
 			myfile.write('{}{}'.format(str(element), '\n'))
 
 
-#######################################################################################################################
+#----------------------------------------------------------------------------------------------------------------------
 # Compareing parsed files with data stored in the CSV format.
-#######################################################################################################################
+#----------------------------------------------------------------------------------------------------------------------
 
 def compare_parsed():
 	dir_1 = os.path.join(CSV_DIR, 'parsed_main')
@@ -147,45 +216,48 @@ def compare_parsed():
 			is_compare_required = True
 
 	if is_compare_required == True:
-		result_from_file_1 = hashlib.sha256(codecs.open(target_1, 'rb').read()).digest()
-		result_from_file_2 = hashlib.sha256(codecs.open(target_2, 'rb').read()).digest()
-		print(result_from_file_1)
-		print(result_from_file_2)
+		result_from_file_1 = hashlib.sha256(open(target_1, 'rb').read()).digest()
+		result_from_file_2 = hashlib.sha256(open(target_2, 'rb').read()).digest()
 		if result_from_file_1 == result_from_file_2:
 			print('TRUE')
+			os.remove(target_2)
 		else:
 			print('FALSE')
+			copyfile(target_2, target_1)
+			os.remove(target_2)
 
 
-#######################################################################################################################
+#----------------------------------------------------------------------------------------------------------------------
 # Utility functions. Dismantle after the development stage.
-#######################################################################################################################
+#----------------------------------------------------------------------------------------------------------------------
 
-def save_work_in_progres(text):
+def save_work_in_progres(a_list):
 	target = os.path.join(BASE_DIR, 'apof\parsed_progres.txt')
 	with codecs.open(target, 'w', 'utf-8') as myfile:
-		for element in text:
+		for element in a_list:
 			myfile.write(str(element))
 
 
-#######################################################################################################################
+#----------------------------------------------------------------------------------------------------------------------
 # Main function of the script.
-#######################################################################################################################
+#----------------------------------------------------------------------------------------------------------------------
 
 def main():
-	processed_data = parse_coloseum(URL)
+	if DEBUG == False:
+		processed_data = parse_coloseum(URL)
+		save_parsed(processed_data)
+		compare_parsed()
+	
+	elif DEBUG == True:
+		bs = read_site(URL)
+		save_work_in_progres(bs)
 
-	#for debuging purposes only.
-	#print(processed_data)
-	
-	#save_parsed(processed_data)
-	current_data = compare_parsed()
-	
 
 if __name__ == '__main__':
 	main()
 
 
-######################################################################3
+######################################################################
+# Disregard that, those are only my notes.
 # len(pizzas) = 177
 # len(pizzas_splited) = 692
